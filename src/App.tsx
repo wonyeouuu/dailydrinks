cbc1db0a163dac5820e5ee4420ed520aa305b54d
陳泓宇 <wonyeouuu@gmail.com>
2019-05-04 17:08:35 +0800

    feat(app): modify root component to use context and previous reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useReducer, createContext } from 'react';
 import './App.css';
+import orderReducer from './store/reducer';
+import { TOrder, TActionType } from './types/Order';
+import OrderList from './components/OrderList';
+
+export const OrderContext = createContext<{
+  orders: TOrder[];
+  dispatch: React.Dispatch<{
+    type: TActionType;
+    params: number | TOrder | { index: number; order: TOrder };
+  }>;
+} | null>(null);
 
 const App: React.FC = () => {
+  const [orders, dispatch] = useReducer(orderReducer, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <OrderContext.Provider value={{ orders, dispatch }}>
+      <OrderList orders={orders}>{order => <div>{order.name}</div>}</OrderList>
+    </OrderContext.Provider>
   );
-}
+};
 
 export default App;
