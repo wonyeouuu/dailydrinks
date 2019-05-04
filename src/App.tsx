ff5a9f1abe6bf7ac08be3a9b89b69c10ae71b939
陳泓宇 <wonyeouuu@gmail.com>
2019-05-04 18:23:51 +0800

    feat(app): implement CRUD logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import React, { useReducer, createContext } from 'react';
+import React, { useReducer, createContext, useState } from 'react';
+import { Button } from 'antd';
 import './App.css';
+import 'antd/dist/antd.css';
 import orderReducer from './store/reducer';
 import { TOrder, TActionType } from './types/Order';
 import OrderList from './components/OrderList';
+import OrderForm from './components/OrderForm';
 
 export const OrderContext = createContext<{
   orders: TOrder[];
@@ -14,10 +17,68 @@ export const OrderContext = createContext<{
 
 const App: React.FC = () => {
   const [orders, dispatch] = useReducer(orderReducer, []);
+  const [isAdding, setIsAdding] = useState<boolean>(false);
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [initialOrder, setInitialOrder] = useState<TOrder | null>(null);
+  const [editingIndex, setEditingIndex] = useState<number | null>(null);
+
+  const handleAddSubmit = (order: TOrder) => {
+    dispatch({ type: 'add', params: order });
+    setIsAdding(false);
+  };
+
+  const handleEditSubmit = (order: TOrder) => {
+    dispatch({
+      type: 'edit',
+      params: { index: editingIndex!, order: order },
+    });
+    setIsEditing(false);
+    setInitialOrder(null);
+    setEditingIndex(null);
+  };
+
+  const makeHandleEditClick = (order: TOrder, index: number) => () => {
+    setIsEditing(true);
+    setInitialOrder(order);
+    setEditingIndex(index);
+  };
+
+  const editFormVisible =
+    isEditing && initialOrder !== null && editingIndex !== null;
 
   return (
     <OrderContext.Provider value={{ orders, dispatch }}>
-      <OrderList orders={orders}>{order => <div>{order.name}</div>}</OrderList>
+      <Button type="primary" icon="plus" onClick={() => setIsAdding(true)}>
+        Add
+      </Button>
+      {isAdding && <OrderForm onSubmit={handleAddSubmit} />}
+      {editFormVisible && (
+        <OrderForm initialOrder={initialOrder!} onSubmit={handleEditSubmit} />
+      )}
+
+      <OrderList orders={orders}>
+        {(order, index) => (
+          <div>
+            <p>name: {order.name}</p>
+            <p>price: {order.price}</p>
+            <pre>
+              notes: <p>{order.notes}</p>
+            </pre>
+            <p>
+              <Button icon="edit" onClick={makeHandleEditClick(order, index)}>
+                Edit
+              </Button>
+              <Button
+                icon="delete"
+                type="danger"
+                onClick={() => dispatch({ type: 'delete', params: index })}
+              >
+                Delete
+              </Button>
+            </p>
+          </div>
+        )}
+      </OrderList>
     </OrderContext.Provider>
   );
 };
--- src/components/OrderList.tsx
+++ src/components/OrderList.tsx
@@ -3,15 +3,15 @@ import { TOrder } from '../types/Order';
 
 type Props = {
   orders: TOrder[];
-  children: (order: TOrder) => React.ReactNode;
+  children: (order: TOrder, index: number) => React.ReactNode;
 };
 
 const OrderList = ({ orders, children }: Props) => (
   <div className="order-list__container">
     <h1 className="order-list__title">Orders List</h1>
     <ul className="order-list__list">
-      {orders.map(order => (
-        <li>{children(order)}</li>
+      {orders.map((order, index) => (
+        <li key={index}>{children(order, index)}</li>
       ))}
     </ul>
   </div>
